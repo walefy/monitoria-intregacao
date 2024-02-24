1071bafe46ccbe52222430292ba1fe80b1b817bd
walefy <walefyd1@gmail.com>
2024-02-24 09:29:18 -0300

    build<backend>: create initial migration

--- app/backend/src/App.ts
+++ app/backend/src/App.ts
@@ -2,29 +2,29 @@ import express from 'express';
 import cors from 'cors';
 
 export class App {
-  public app: express.Express;
+	public app: express.Express;
 
-  constructor() {
-    this.app = express();
-    this.config();
-    this.routes();
-  }
+	constructor() {
+		this.app = express();
+		this.config();
+		this.routes();
+	}
 
-  private config(): void {
-    this.app.use(express.json());
-    this.app.use(cors({
-      origin: '*',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    }));
-  }
+	private config(): void {
+		this.app.use(express.json());
+		this.app.use(cors({
+			origin: '*',
+			methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		}));
+	}
 
-  private routes(): void {
-    console.warn('Method not implemented.');
-  }
+	private routes(): void {
+		console.warn('Method not implemented.');
+	}
 
-  public start(port: number): void {
-    this.app.listen(port, () => {
-      console.log(`Server listening on port ${port}`);
-    });
-  }
+	public start(port: number): void {
+		this.app.listen(port, () => {
+			console.log(`Server listening on port ${port}`);
+		});
+	}
 }
